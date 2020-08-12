5caf673f6781ec98e497955e2a114670be0715b3
Patrick-G-Oliver <patrickgriffitholiver@gmail.com>
2020-08-11 21:01:29 -0400

    useLocalStorage hook completed, useDarkMode hook begun

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -5,6 +5,13 @@ export const useLocalStorage = (key, initialValue) => {
         const item = window.localStorage.getItem(key);
         return item ? JSON.parse(item) : initialValue;
     });
-    
-    return initialValue;
+
+    const setValue = value => {
+        // save state
+        setStoredValue(value);
+        // save to local storage
+        window.localStorage.setItem(key, JSON.stringify(value));
+    }
+
+    return [storedValue, setValue];
 }
